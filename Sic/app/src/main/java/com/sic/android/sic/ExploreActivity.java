cacf80ab39cea95c498c37c43fed5416eb1c994e
andeyt <jtuttle5@uwyo.edu>
2018-10-15 16:07:26 -0600

    Add preliminary listview

--- Sic/app/src/main/java/com/sic/android/sic/ExploreActivity.java
+++ Sic/app/src/main/java/com/sic/android/sic/ExploreActivity.java
@@ -1,16 +1,14 @@
 package com.sic.android.sic;
 
-import android.content.Intent;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
-public class ExploreActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
+public class ExploreActivity extends ListActivity {
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -19,14 +17,15 @@ public class ExploreActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_explore:
-                    mTextMessage.setText("Already in " + R.string.title_explore);
+
+                    Toast.makeText(ExploreActivity.this, "Already on tab Explore", Toast.LENGTH_LONG).show();
 
                     return true;
                 case R.id.navigation_training:
-                    mTextMessage.setText(R.string.title_training);
+                    Toast.makeText(ExploreActivity.this, "Training coming soon!", Toast.LENGTH_LONG).show();
                     return true;
                 case R.id.navigation_about:
-                    mTextMessage.setText(R.string.title_about);
+                    Toast.makeText(ExploreActivity.this, "About coming soon!", Toast.LENGTH_LONG).show();
                     return true;
             }
             return false;
@@ -38,7 +37,17 @@ public class ExploreActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explore);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        String[] itemName = {
+                getString(R.string.techName0),
+                getString(R.string.techName1)
+        };
+
+        int[] itemPics = {
+            R.drawable.ic_home_black_24dp,
+            R.drawable.ic_dashboard_black_24dp,
+        };
+
+        this.setListAdapter(new ArrayAdapter<String>(this, R.layout.my_list, R.id.Itemname, itemName));
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
